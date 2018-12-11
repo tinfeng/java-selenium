1c134bb854bdde4d1d217bbcd3bb2477a65af48f
tinfeng <624783279@qq.com>
2018-12-11 15:19:44 +0800

    重新编写了从确认订单到退货成功的流程,新增PC端下单流程

---
+++ src/PC.java
@@ -0,0 +1,125 @@
+import java.util.*;
+import java.util.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
+import java.util.Set;
+import org.junit.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import static org.junit.Assert.*;
+
+public class PC {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+        //配置并打开浏览器
+        System.setProperty("webdriver.chrome.driver", "D:\\JAVA\\chromedriver.exe");
+        driver = new ChromeDriver();
+        //设置浏览器大小
+        driver.manage().window().setSize(new Dimension(1280, 800));
+        //测试地址
+        baseUrl = "http://139.159.241.7:9061";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testUntitledTestCase() throws Exception {
+        //进入首页
+        driver.get(baseUrl + "/index.html");
+        //添加cookie
+        driver.manage().addCookie(new Cookie("SESSION", "492c6f73-205c-4547-a281-6fd61be5aa44"));
+        //刷新页面进行登陆
+        driver.navigate().refresh();
+        //搜索关键字落叶
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='更多优惠等你'])[1]/following::input[1]")).sendKeys("落叶");
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='更多优惠等你'])[1]/following::img[2]")).click();
+        //按销量排序
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='销量'])[1]/following::span[1]")).click();
+        Thread.sleep(5000);
+        //鼠标停留在'折后约'上
+        WebElement card = driver.findElement(By.xpath("/html/body/div[1]/section/main/div/div[3]/ul/li[1]/div/div[1]/div/h5/small"));
+        Actions action = new Actions(driver);
+        action.moveToElement(card).perform();
+        Thread.sleep(2000);
+        //将价格最低的商品加入购物车
+        driver.findElement(By.id("addCart")).click();
+        //进入购物车
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='更多优惠等你'])[1]/following::button[1]")).click();
+        //切换标签页
+        Set<String> winHandels = driver.getWindowHandles(); // 得到当前窗口的set集合
+        List<String> it = new ArrayList<String>(winHandels); // 将set集合存入list对象
+        driver.switchTo().window(it.get(1)); // 切换到弹出的新窗口
+        Thread.sleep(1000);
+        //点击结算
+        driver.findElement(By.xpath("/html/body/div/section/main/div/div/div[3]/button")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div[1]/div[1]/div/div[2]/div/div[2]/button[1]")).click();
+        //点击提交订单
+        driver.findElement(By.xpath("/html/body/div[1]/section/main/div/div/div[4]/div/button")).click();
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='以下商家未满足最小配送金额，是否继续？'])[1]/following::button[1]")).click();
+        //立即支付
+        driver.findElement(By.xpath("/html/body/div[1]/section/main/div/div[3]/button")).click();
+        Thread.sleep(28000);
+        WebElement flag = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/span"));
+        if (flag != null)
+        {
+            System.out.println("未支付");
+            driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div/button[2]")).click();
+
+        }
+        else {
+            System.out.println("下单成功,返回订单页面");
+            driver.findElement(By.xpath("//*[@id=\"app\"]/section/main/div[2]/button[1]")).click();
+        }
+        Thread.sleep(10000);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        //driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
---
+++ src/supplier.java
@@ -0,0 +1,128 @@
+import java.util.*;
+import java.util.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
+import java.util.Set;
+import org.junit.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import static org.junit.Assert.fail;
+
+public class supplier {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+        //配置并打开浏览器
+        System.setProperty("webdriver.chrome.driver", "D:\\JAVA\\chromedriver.exe");
+        driver = new ChromeDriver();
+        //设置浏览器大小
+        driver.manage().window().setSize(new Dimension(1280, 800));
+        //测试地址
+        baseUrl = "http://139.159.241.7:9041";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        //进入首页
+        driver.get(baseUrl + "/login.html");
+        //添加cookie
+        driver.manage().addCookie(new Cookie("SESSION", "3202b86b-c3b9-4938-bcad-925af2bf9759"));
+        Thread.sleep(2000);
+        //刷新页面进行登陆
+        driver.get(baseUrl + "/index.html");
+        //driver.navigate().refresh();
+
+    }
+
+    @Test
+    public void testUntitledTestCase() throws Exception {
+
+        driver.findElement(By.linkText("销售管理")).click();
+        Thread.sleep(500);
+        driver.findElement(By.linkText("订单列表")).click();
+        Thread.sleep(2000);
+        driver.switchTo().frame(1);
+
+        driver.findElement(By.linkText("确认订单")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("html/body/div[5]/div[2]/div/div[3]/button[1]")).click();
+
+        driver.findElement(By.linkText("确认发货")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div[7]/div[2]/div/div[2]/div[2]/div/input")).sendKeys("123321");
+        Thread.sleep(500);
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='快递单号'])[1]/following::button[1]")).click();
+
+        driver.findElement(By.linkText("确认收货")).click();
+        Thread.sleep(500);
+        driver.findElement(By.xpath("//*[@id=\"confirm-goods\"]/div[2]/div/div[3]/button[1]")).click();
+
+        driver.findElement(By.linkText("申请退款")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//*[@id=\"applyRefund\"]/div[2]/div/div[2]/div[1]/div[3]/div[2]/textarea")).sendKeys("退款");
+        String nub = driver.findElement(By.xpath("//*[@id=\"goodsList\"]/tr/td[3]")).getText();
+        driver.findElement(By.xpath("//*[@id=\"goodsList\"]/tr/td[5]/input")).sendKeys(nub);
+        driver.findElement(By.xpath("//*[@id=\"applyRefund\"]/div[2]/div/div[3]/button[1]")).click();
+
+        driver.switchTo().defaultContent();
+        driver.findElement(By.linkText("售后订单")).click();
+        Thread.sleep(2000);
+        driver.switchTo().frame(2);
+
+        driver.findElement(By.xpath("/html/body/div[1]/div[2]/table/tbody/tr[1]/td[10]/div/a[2]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//*[@id=\"update-status\"]/div[2]/div/div[3]/button[1]")).click();
+
+        driver.findElement(By.linkText("确认收货")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//*[@id=\"update-status\"]/div[2]/div/div[3]/button[1]")).click();
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
+
