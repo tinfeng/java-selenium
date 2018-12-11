11ada6c83613cb1311c68ee5afd5024d4d0eef15
tinfeng <624783279@qq.com>
2018-12-11 18:02:12 +0800

    增加注释

--- src/supplier.java
+++ src/supplier.java
@@ -1,13 +1,9 @@
-import java.util.*;
 import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
-import java.util.Set;
 import org.junit.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 
 import static org.junit.Assert.fail;
 
@@ -35,32 +31,35 @@ public class supplier {
         //刷新页面进行登陆
         driver.get(baseUrl + "/index.html");
         //driver.navigate().refresh();
-
     }
 
     @Test
     public void testUntitledTestCase() throws Exception {
-
+        //进入订单列表
         driver.findElement(By.linkText("销售管理")).click();
         Thread.sleep(500);
         driver.findElement(By.linkText("订单列表")).click();
         Thread.sleep(2000);
-        driver.switchTo().frame(1);
+        driver.switchTo().frame(1);     //切换frame
 
+        //确认订单
         driver.findElement(By.linkText("确认订单")).click();
         Thread.sleep(1000);
         driver.findElement(By.xpath("html/body/div[5]/div[2]/div/div[3]/button[1]")).click();
 
+        //确认发货
         driver.findElement(By.linkText("确认发货")).click();
         Thread.sleep(1000);
         driver.findElement(By.xpath("/html/body/div[7]/div[2]/div/div[2]/div[2]/div/input")).sendKeys("123321");
         Thread.sleep(500);
         driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='快递单号'])[1]/following::button[1]")).click();
 
+        //确认收货
         driver.findElement(By.linkText("确认收货")).click();
         Thread.sleep(500);
         driver.findElement(By.xpath("//*[@id=\"confirm-goods\"]/div[2]/div/div[3]/button[1]")).click();
 
+        //申请退款
         driver.findElement(By.linkText("申请退款")).click();
         Thread.sleep(1000);
         driver.findElement(By.xpath("//*[@id=\"applyRefund\"]/div[2]/div/div[2]/div[1]/div[3]/div[2]/textarea")).sendKeys("退款");
@@ -68,19 +67,20 @@ public class supplier {
         driver.findElement(By.xpath("//*[@id=\"goodsList\"]/tr/td[5]/input")).sendKeys(nub);
         driver.findElement(By.xpath("//*[@id=\"applyRefund\"]/div[2]/div/div[3]/button[1]")).click();
 
-        driver.switchTo().defaultContent();
+        driver.switchTo().defaultContent();     //跳出frame
         driver.findElement(By.linkText("售后订单")).click();
         Thread.sleep(2000);
-        driver.switchTo().frame(2);
+        driver.switchTo().frame(2);     //进入售后订单frame
 
+        //同意退款
         driver.findElement(By.xpath("/html/body/div[1]/div[2]/table/tbody/tr[1]/td[10]/div/a[2]")).click();
         Thread.sleep(1000);
         driver.findElement(By.xpath("//*[@id=\"update-status\"]/div[2]/div/div[3]/button[1]")).click();
 
+        //确认收货
         driver.findElement(By.linkText("确认收货")).click();
         Thread.sleep(1000);
         driver.findElement(By.xpath("//*[@id=\"update-status\"]/div[2]/div/div[3]/button[1]")).click();
-
     }
 
     @After
